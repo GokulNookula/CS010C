7a70cacf35f1d3bfea3a53173b3be6547721d66c
GokulNookula <111548159+GokulNookula@users.noreply.github.com>
2023-04-10 20:04:37 -0700

    Update Playlist.h

--- Lab 1/Playlist.h
+++ Lab 1/Playlist.h
@@ -1,28 +1,40 @@
 #ifndef Playlist_H
 #define Playlist_H
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-
 class PlaylistNode
 {
     public:
 
+        // Default constructor
         PlaylistNode();
-        PlaylistNode(string, string, string, int);
-        void InsertAfter(PlaylistNode*);
-        void SetNext(PlaylistNode*);
-        string GetID()const;
+
+        // Parameterized constructor
+        PlaylistNode(string id, string songName, string artistName, int length);
+
+        // Insert a node after this node
+        void InsertAfter(PlaylistNode* node);
+
+        // Set the next node pointer
+        void SetNext(PlaylistNode* node);
+
+        // Getter methods for private member variables
+        string GetID() const;
         string GetArtistName() const;
         string GetSongName() const;
         int GetSongLength() const;
         PlaylistNode* GetNext() const;
+
+        // Print the details of this node
         void PrintPlaylistNode();
 
     private:
 
+        // Private member variables
         string uniqueID;
         string songName;
         string artistName;
@@ -33,19 +45,34 @@ class PlaylistNode
 class Playlist
 {
     public: 
-        Playlist(string);
+
+        // Parameterized constructor
+        Playlist(string playlistTitle);
+
+        // Output the full playlist
         void OutputFullPlaylist();
+
+        // Output all songs by a specific artist
         void OutputSongsSpecificArtist();
+
+        // Output the total time of the playlist in seconds
         void OutputTotalTime();
+
+        // Add a song to the playlist
         void AddSong();
+
+        // Remove a song from the playlist
         void RemoveSong();
+
+        // Change the position of a song in the playlist
         void ChangePosition();
 
     private:
+
+        // Private member variables
         string title;
         PlaylistNode* head;
         PlaylistNode* tail;
 };
 
 #endif
-
