fabfa03cf6f4ff4256753885074f1af4ce4550e9
GokulNookula <111548159+GokulNookula@users.noreply.github.com>
2023-04-26 18:33:12 -0700

    Creating a stack from scratch

---
+++ Lab 4/stack.h
@@ -0,0 +1,71 @@
+#ifndef STACK_H
+#define STACK_H
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+const int MAX_SIZE = 20;
+
+using namespace std;
+
+template <typename T>
+class stack
+{
+    public:
+        stack()
+        {
+            _data = new T[MAX_SIZE];
+            _size = 0;
+        }
+        ~stack()
+        {
+            delete[] _data;
+            _size = 0;
+        }
+        void push(T val)
+        {
+            if (_size < MAX_SIZE)
+            {
+                _data[_size] = val;
+                _size++;
+                return;
+            }
+            throw overflow_error ("Called push on full stack.");
+        }
+        void pop()
+        {
+            if (_size > 0)
+            {
+                _size--;
+            }
+            throw out_of_range ("Called pop on empty stack.");
+        }
+        void pop_two()
+        {
+            if (_size > 1)
+            {
+                _size -= 2;
+            }
+            throw out_of_range ("Called pop_two on empty stack.");
+        }
+        const T top()
+        {
+            if (!empty())
+            {
+                return _data[_size - 1];
+            }
+            throw underflow_error ("Called top on empty stack.");
+            return 0;
+        }
+        const bool empty()
+        {
+            return (_size == 0);
+        }
+
+
+    private:
+        T* _data;
+        int _size;
+};
+
+#endif
